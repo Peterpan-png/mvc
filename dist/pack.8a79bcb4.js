38fff09352c9df73a828b33891b3a7557ab9205f
peter <948430126@qq.com>
2020-10-12 18:35:57 +0800

    调试

--- dist/pack.8a79bcb4.js
+++ dist/pack.8a79bcb4.js
@@ -11419,7 +11419,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3379" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4694" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
